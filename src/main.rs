067a09c5d234cc237e6f521711aa2182250cf681
Ethan House <ehouse@palantir.com>
2018-03-18 10:25:28 -0400

    Updated logic and included panic checks

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use rand::Rng;
 use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 
+const SIZE_OF_HEAP: usize = 4096;
+
 fn hash(val: &str) -> String{
     let mut hasher = Sha256::new();
     hasher.input_str(&val);
@@ -20,32 +22,42 @@ fn verify_hash(val: &str, hashed_val: &str) -> bool{
     return false;
 }
 
+fn verify_vec_hash(vec: &[String], hashed_vec: &[String]) -> Result<i32, String> {
+    for x in 0 .. SIZE_OF_HEAP+1{
+        if hash(&vec[x]) == hashed_vec[x] {
+            return Ok(0);
+        }
+        return Err("Vector mismatch!".to_string());
+    }
+    return Err("Improper vector size".to_string());
+}
+
 fn main() {
 
     // count logical cores this process could try to use
     // let num = num_cpus::get();
     // println!("There are {} Logical CPU's", num);
 
-    let size_of_heap = 64;
-
     let mut rng = rand::thread_rng();
 
-    let mut val_heap: Vec<String> = Vec::new();
-    let mut hash_heap: Vec<String> = Vec::new();
+    let mut val_heap: Vec<String> = Vec::with_capacity(SIZE_OF_HEAP);
+    let mut hash_heap: Vec<String> = Vec::with_capacity(SIZE_OF_HEAP);
 
     let mut random_val: u64;
 
-    for _ in 0 .. size_of_heap+1{
+    for _ in 0 .. SIZE_OF_HEAP+1{
         random_val = rng.gen();
         val_heap.push(random_val.to_string());
     }
 
-    for x in 0 .. size_of_heap+1{
+    for x in 0 .. SIZE_OF_HEAP+1{
         hash_heap.push(hash(&val_heap[x]));
     }
 
-    println!("{} , {}", val_heap[0], val_heap[1]);
-    println!("{} , {}", hash_heap[0], hash_heap[1]);
-    println!("Hashes match: {}", verify_hash(&val_heap[0], &hash_heap[0]));
+    let status = verify_vec_hash(&val_heap, &hash_heap);
+    match status{
+        Ok(_)  => println!("Vectors Match!"),
+        Err(e) => panic!("ERROR: {}",e),
+    }
 }
 
